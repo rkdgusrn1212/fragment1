492a74841cd77c9503bab0832d405efc48ef8286
rkdgusrn1212 <khgkjg12@naver.com>
2017-09-05 22:24:47 +0900

    Initial commit

---
+++ app/src/main/java/com/khgkjg12/examples/fragment1/BookListFragment.java
@@ -0,0 +1,30 @@
+package com.khgkjg12.examples.fragment1;
+
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class BookListFragment extends Fragment {
+
+
+    public BookListFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View viewHierarchy = inflater.inflate(R.layout.fragment_book_list, container, false);
+        return viewHierarchy;
+    }
+
+}
